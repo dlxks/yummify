3f8b210ff015b3c67ba0a880e8679e7e17f93993
dlxks <dlxks.sangangbayan@gmail.com>
2025-08-11 16:03:29 +0800

    Revamp styles for a comic-themed UI, enhancing typography and layout across components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import { Icon } from "@iconify/react/dist/iconify.js"
 
 function Header() {
-
   return (
     <header>
-      {/* <img src={chefClaudeLogo} alt="Chef Claude Logo" /> */}
       <Icon icon="streamline-ultimate-color:chef-gear-gloves" width="45" height="45" />
       <h1>Yummify</h1>
     </header>
   )
-
 }
 
 export default Header
